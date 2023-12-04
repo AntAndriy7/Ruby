5bc39caa05e6653db66c95401f42877b7400b93e
AntAndriy7 <andriy.antonov7@gmail.com>
2023-12-04 15:18:05 +0200

    Variant 2

---
+++ Test_04.12.2003/test.rb
@@ -0,0 +1,81 @@
+class Event
+  attr_accessor :title, :date
+
+  def initialize(title, date)
+    @title = title
+    @date = date
+  end
+
+  def to_s
+    "#{@title} - #{@date}"
+  end
+end
+
+class Organizer
+  attr_accessor :events
+
+  def initialize
+    @events = []
+  end
+
+  def add_event(event)
+    @events << event
+  end
+
+  def sort(start_date = nil, end_date = nil)
+    sorted_events = @events.sort_by(&:date)
+
+    if start_date && end_date
+      sorted_events.select { |event| event.date.between?(start_date, end_date) }
+    else
+      sorted_events
+    end
+  end
+
+  def search_by_date(target_date)
+    @events.select { |event| event.date == target_date }
+  end
+
+  def reminders(today)
+    upcoming_events = @events.select { |event| event.date >= today }
+    if upcoming_events.empty?
+      puts "\nNo upcoming reminders."
+    else
+      puts "\nUpcoming reminders:"
+      upcoming_events.each do |event|
+        puts "Reminder: #{event.title} is coming up on #{event.date}"
+      end
+    end
+  end
+end
+
+organizer = Organizer.new
+
+event1 = Event.new("Meeting", "2023-12-10")
+event2 = Event.new("New Year", "2023-12-31")
+event3 = Event.new("Test", "2023-12-04")
+
+organizer.add_event(event1)
+organizer.add_event(event2)
+organizer.add_event(event3)
+
+puts "Sorted Events:"
+sorted = organizer.sort
+sorted.each { |event| puts event }
+
+puts "\nSorted Events in Range:"
+sorted_in_range = organizer.sort("2023-11-01", "2023-12-20")
+sorted_in_range.each { |event| puts event }
+
+puts "\nEvents on 2023-12-10:"
+searched = organizer.search_by_date("2023-12-10")
+searched.each { |event| puts event }
+
+today = "2023-10-01"
+organizer.reminders(today)
+
+today = "2023-12-20"
+organizer.reminders(today)
+
+today = "2024-01-01"
+organizer.reminders(today)
